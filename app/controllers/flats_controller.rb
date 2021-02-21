f6d38724a40b9f533ef859ff4913859ff8e48938
James Wordie <james.wordie@yahoo.com>
2021-02-21 14:47:08 +0000

    finished apart from search

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :find_flat, only: %i[create show destroy]
+  before_action :find_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     @flats = Flat.all
@@ -11,22 +11,27 @@ class FlatsController < ApplicationController
 
   def create
     @flat = Flat.new(strong_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def show
-
   end
 
   def update
-
+    @flat.update(strong_params)
+    redirect_to flat_path(@flat)
   end
 
   def destroy
-
+    @flat.destroy
+    redirect_to root_path
   end
 
   private
